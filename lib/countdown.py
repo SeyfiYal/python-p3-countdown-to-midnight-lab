3dac344a78fe7d41fb9d6886ffb6103b75a9aaf8
Seyfican Yalcindag <+>
2023-04-04 16:07:31 +0300

    countdown

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,20 @@
 # your code goes here!
+
+
+def countdown(num):
+    while num > 0:
+        print(f"{num} SECOND(S)!")
+        num -= 1
+    print("HAPPY NEW YEAR!")
+countdown(10)
+
+
+import time
+
+def countdown_with_sleep(num):
+    while num > 0:
+        print(f"{num} SECOND(S)!")
+        num -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+countdown_with_sleep(10)
